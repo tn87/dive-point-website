376e94a53426739f7661ac258c01114198b9d1aa
tn87 <117167748+tn87@users.noreply.github.com>
2024-01-12 20:59:07 +0200

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -3,16 +3,16 @@ import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 export const onRequest: PagesFunction = mailChannelsPlugin({
   personalizations: [
     {
-      to: [{ name: "ACME Support", email: "mail@tn87.de" }],
+      to: [{ name: "DP-Booking", email: "divepoint@tn87.de" }],
     },
   ],
   from: {
-    name: "ACME Support",
+    name: "Dive-Point-Webmailer",
     email: "support@d1ve.xyz",
   },
   respondWith: () => {
     return new Response(
-      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+      `Thank you!`
     );
   },
 });
