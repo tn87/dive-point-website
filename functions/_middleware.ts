3c23fddb93175efcfd342979928c1dc5cbde94b2
tn87 <service@tn87.de>
2024-01-12 17:25:06 +0200

    .

---
+++ functions/_middleware.ts
@@ -0,0 +1,15 @@
+import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest = mailchannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "ACME Support", email: "mail@tn87.de" }],
+    },
+  ],
+  from: { name: "Enquiry", email: "no-replyd1ve.xyz" },
+  respondWith: () =>
+    new Response(null, {
+      status: 302,
+      headers: { Location: "/thank-you" },
+    }),
+});
