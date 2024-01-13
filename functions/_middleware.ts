9eb2169407bfed1aed74d222e84ec7cdc3b5705c
tn87 <service@tn87.de>
2024-01-13 10:47:27 +0200

    .

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,30 +1,18 @@
 // https://ubuverse.com/deploying-a-static-website-to-cloudflare-pages/
 
-// This is the demo secret key. In production, we recommend
-// you store your secret key(s) safely.
-const SECRET_KEY = "0x4AAAAAAAP5xZtd2fj0PmLQzLLMC-VtDBE";
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-async function handlePost(request) {
-  const body = await request.formData();
-  // Turnstile injects a token in "cf-turnstile-response".
-  const token = body.get("cf-turnstile-response");
-  const ip = request.headers.get("CF-Connecting-IP");
-
-  // Validate the token by calling the
-  // "/siteverify" API endpoint.
-  let formData = new FormData();
-  formData.append("secret", SECRET_KEY);
-  formData.append("response", token);
-  formData.append("remoteip", ip);
-
-  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
-  const result = await fetch(url, {
-    body: formData,
-    method: "POST",
-  });
-
-  const outcome = await result.json();
-  if (outcome.success) {
+export const onRequest: PagesFunction = mailChannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "DP-Booking", email: "divepoint@tn87.de" }],
+    },
+  ],
+  from: {
+    name: "Dive-Point-Webmailer",
+    email: "support@d1ve.xyz",
+  },
+  respondWith: () => {
     return Response.redirect("https://d1ve.xyz/thankyou/", 302);
-  }
-}
+  },
+});
